da7898cfeeea6ca48d125dd95c991fe0a654dc52
Mohanambal25 <30648223+Mohanambal25@users.noreply.github.com>
2017-08-08 09:32:39 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
-console.log('Loaded!');
+//counter code
+ var counter;
+document.getElementById("counter").onclick(function(){
+ 
+    
+    
+    
+    
+    counter=counter+1;
+    document.getElementById("count").innerHTML=counter.toString();
+    
+    
+});
