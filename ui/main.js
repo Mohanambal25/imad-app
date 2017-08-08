ed4f71f97cd7377f4d1b570991b99a15c114ecfd
Mohanambal25 <30648223+Mohanambal25@users.noreply.github.com>
2017-08-08 09:48:47 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,32 @@
 //counter code
-
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function(){
+    
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        
+        if(request.readyState===XMLHttpRequest){
+            
+            if(request.status===200){
+                
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+            
+        }
+        
+        
+        
+    };
+    
+    request.open('GET','http://mohanambal25.imad.hasura-app.io/',true);
+    request.send(null);
+    
 
     
     
     
-    
-    counter= counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
-    
-}
+};
